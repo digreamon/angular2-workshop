d59318296ea9190bcf1b8a54dad9fbb9a6263ffb
Dmytro Kolomiye@gmail.comts <digreamon>
2017-03-30 14:34:03 +0300

    minor fixes

--- gulpfile.js
+++ gulpfile.js
@@ -7,69 +7,67 @@ var gulp = require("gulp"),
 
 var tsProject = ts.createProject("src/tsconfig.json");
 
-gulp.task("compile", function () {
-  var tsResult = tsProject.src()
-  .pipe(sourcemaps.init())
-  .pipe(tsProject());
+var doServe = function () {
+    // verbose execution, with lows
 
-  return tsResult.js
-  .pipe(sourcemaps.write(".")) // produce *.js.map files
-  .pipe(gulp.dest("dist"));
-});
-
-gulp.task('webpack', ["compile"], function () {
-  return gulp.src('dist/main.js')
-  .pipe(webpack(require('./webpack.config.js')))
-  .pipe(gulp.dest('dist/'));
-});
-
-gulp.task("watch", ["compile"], function () {
-  gulp.watch(["src/**/*.ts"], ["compile"]).on("change", function (e) {
-    console.log("TypeScript file " + e.path + " has been changed. Refreshing.");
-  });
-});
-
-gulp.task("watch-webpack", ["webpack"], function () {
-  gulp.watch(["src/**/*.ts", "webpack.config.js"], ["webpack"]).on("change", function (e) {
-    console.log("TypeScript file " + e.path + " has been changed. Refreshing.");
-  });
-});
-
-gulp.task("serve", ["compile"], function (cb) {
-  // no logs, silent execution
-  // exec is not meant for processes that return HUGE buffers to Node. You should use spawn for that. So what do you use exec for? Use it to run programs that return result statuses, instead of data.
-  // exec("yarn run lite", function (err, stdout, stderr) {
-  //   console.log(stdout);
-  //   console.log(stderr);
-  //   cb(err);
-  // });
-
-  // verbose execution, with lows
-  {
     var ls = spawn("yarn", ["run", "lite"]);
 
     ls.stdout.on("data", function (data) {
-      console.log("stdout: " + data.toString());
+        console.log("stdout: " + data.toString());
     });
 
     ls.stderr.on("data", function (data) {
-      console.log("stderr: " + data.toString());
+        console.log("stderr: " + data.toString());
     });
 
     ls.on("exit", function (code) {
-      console.log("child process exited with code " + code.toString());
+        console.log("child process exited with code " + code.toString());
     });
-  }
+}
+
+gulp.task("compile", function () {
+    var tsResult = tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
+
+    return tsResult.js
+        .pipe(sourcemaps.write(".")) // produce *.js.map files
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task('webpack', ["compile"], function () {
+    return gulp.src('dist/main.js')
+        .pipe(webpack(require('./webpack.config.js')))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task("watch", ["compile"], function () {
+    gulp.watch(["src/**/*.ts"], ["compile"]).on("change", function (e) {
+        console.log("TypeScript file " + e.path + " has been changed. Refreshing.");
+    });
+});
+
+gulp.task("watch-webpack", ["webpack"], function () {
+    gulp.watch(["src/**/*.ts", "webpack.config.js"], ["webpack"]).on("change", function (e) {
+        console.log("TypeScript file " + e.path + " has been changed. Refreshing.");
+    });
+});
+
+gulp.task("serve", ["compile"], function (cb) {
+    // no logs, silent execution
+    // exec is not meant for processes that return HUGE buffers to Node. You should use spawn for that. So what do you use exec for? Use it to run programs that return result statuses, instead of data.
+    // exec("yarn run lite", function (err, stdout, stderr) {
+    //   console.log(stdout);
+    //   console.log(stderr);
+    //   cb(err);
+    // });
+
+    doServe()
 
 });
 
-gulp.task("serve-webpack", ["webpack"], function (cb) {
-  // no logs, silent execution
-  exec("yarn run lite", function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
+gulp.task("serve-webpack", ["webpack"], function () {
+    doServe();
 });
 
 gulp.task("build-webpack", ["watch-webpack", "serve-webpack"], function () {
